c012def3ded1a35f19fe85a30744cc3e7b84707e
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2021-09-13 14:53:22 -0300

    Layout Ideas for Demos updates

--- src/app/backup/search-results1/search-results1.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-results1',
-  templateUrl: './search-results1.component.html',
-  styleUrls: ['./search-results1.component.scss']
-})
-export class SearchResults1Component {
-}
